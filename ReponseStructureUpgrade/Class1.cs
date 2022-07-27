31d4230c6400cafb9af85a58d00e2e5440037c25
Reponse Mod <cinarbenliiphone@hotmail.com>
2022-07-27 19:30:55 +0300

    Proje dosyası ekle.

---
+++ ReponseStructureUpgrade/Class1.cs
@@ -0,0 +1,84 @@
+﻿using Rocket.API;
+using Rocket.Core.Commands;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Events;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ReponseStructureUpgrade
+{
+    public class Class1 : RocketPlugin
+    {
+        protected override void Load()
+        {
+            base.Load();
+            UnturnedPlayerEvents.OnPlayerUpdateGesture += sa;
+        }
+
+    
+
+        private void sa(UnturnedPlayer player, UnturnedPlayerEvents.PlayerGesture gesture)
+        {
+            if (gesture == UnturnedPlayerEvents.PlayerGesture.PunchRight)
+            {
+                if (Physics.Raycast(player.Player.look.aim.position, player.Player.look.aim.forward, out var hit, 10f, RayMasks.STRUCTURE_INTERACT))
+                {
+                  
+                    var structure = StructureManager.FindStructureByRootTransform(hit.transform);
+                    var data = structure.GetServersideData();
+
+                  
+                    
+
+                    
+
+                    switch (structure.asset.id)
+                    {
+                        case 3711:
+                            StructureManager.dropStructure(new Structure(3712), data.point, data.angle_x * 2, data.angle_y * 2, data.angle_z * 2, player.CSteamID.m_SteamID, player.Player.quests.groupID.m_SteamID);
+                            StructureManager.damage(hit.transform, structure.model.position, structure.asset.health, 3, true);
+                            break;
+                        case 3712:
+                            StructureManager.dropStructure(new Structure(3713), data.point, data.angle_x * 2, data.angle_y * 2, data.angle_z * 2, player.CSteamID.m_SteamID, player.Player.quests.groupID.m_SteamID);
+                            StructureManager.damage(hit.transform, structure.model.position, structure.asset.health, 3, true);
+                            break;
+                        case 3713:
+                            StructureManager.dropStructure(new Structure(3714), data.point, data.angle_x * 2, data.angle_y * 2, data.angle_z * 2, player.CSteamID.m_SteamID, player.Player.quests.groupID.m_SteamID);
+                            StructureManager.damage(hit.transform, structure.model.position, structure.asset.health, 3, true);
+                            break;
+
+
+                    }
+                 
+
+
+                }
+                return;
+            }
+            else if (gesture == UnturnedPlayerEvents.PlayerGesture.Point)
+            {
+                if (Physics.Raycast(player.Player.look.aim.position, player.Player.look.aim.forward, out var hit, 10f, RayMasks.STRUCTURE_INTERACT))
+                {
+                    var structure = StructureManager.FindStructureByRootTransform(hit.transform);
+                    var data = structure.GetServersideData();
+
+                    UnturnedChat.Say (data.structure.health + data.owner.ToString());
+                
+
+
+                }
+                return;
+            }
+            return;
+        }
+
+    }
+}
---
+++ ReponseStructureUpgrade/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("ReponseStructureUpgrade")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReponseStructureUpgrade")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("b452ca2e-373c-4b4d-b1e8-c1e4e2c4bedb")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
